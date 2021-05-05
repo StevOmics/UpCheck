6350141888a8b5fefc91ea20c6a9bf437fe15f21
Steve Ayers <californiastephen@outlook.com>
2021-05-04 17:11:14 -0700

    better arg parsing

--- upcheck.py
+++ upcheck.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 from time import sleep
 import json
@@ -6,27 +7,12 @@ from func_timeout import func_timeout, FunctionTimedOut , func_set_timeout
 from datetime import datetime
 import argparse
 version = '1.0.0'
-timeout = 5
+timeout = 10
 
 def load_sites():
     with open("sites.json","r") as sites_file:
         site_list = json.load(sites_file)
         return site_list
-
-@func_set_timeout(10)
-def site_is_down(url=""):
-    print("Trying to connect to: "+url)
-    try:
-        res = requests.get(url,timeout=timeout)
-        if(res.status_code==200):
-            print("Able to connect.")
-        else:
-            print("Something went wrong... "+res.status_code)
-        return False
-    except (requests. ConnectionError, requests. Timeout) as exception:
-        print(exception)
-        print("Unable to connect")
-        return True
 
 def send_alert(subject,message):
     with open("env.json") as env:
@@ -38,55 +24,119 @@ def send_alert(subject,message):
                 email_alerts.send(auth=creds,subject=subject,message=message,destination=contact['email'])
     print("Sending alert!")
 
-def monitor(site_list,interval=60):
-    while(True):
-        for site in site_list:
-            print("""*** Checking site: {} *** """.format(site['name']))
-            default_ports=['80','443']
-            if('ports' in site):
-                ports = site['ports']
-                if(len(ports)==0): ports = default_ports
-            else:
-                ports = default_ports
-            for port in ports:
-                if(port in ['443']):
-                    http = "https"
-                else:
-                    http = "http"
-                url = """{}://{}:{}""".format(http,site['url'],port)
-                print("Checking: "+url)
-                down = False
-                try:
-                    down = site_is_down(url)
-                except FunctionTimedOut:
-                    print("Timed out connecting to site.")
-                    down = True
-                if(down):
-                    now = datetime.now()
-                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-                    subject = """[ALERT]Site '{}' is down""".format(site['name'])
-                    message = """Unable to connect to site: {}\nAt: {}\nURL: {}
-                    """.format(site['name'],dt_string,url)
-                    send_alert(subject,message)
-                else:
-                    print("Site is OK.")
-        print("""Sleeping for {} seconds""".format(str(interval)))        
-        sleep(interval) 
+@func_set_timeout(30)
+def url_down(url):
+    res = requests.get(url,timeout=timeout)
+    if(res.status_code==200):
+        print("URL verified: "+url)
+        # return res.status_code
+        return False
+    else:
+        print("Something went wrong... "+res.status_code)
+        # return res.status_code
+        return True
 
+def format_url(url,http="http",port=None):
+    url = url.lower()
+    if('http' not in url):
+        url = http+"://"+url
+    if(port):
+        url = url + ":"+port
+    return url
+
+def check_site(site,retries = None,email=False):
+    if(not retries):retries = 1
+    down = True   
+    message = "Status for site: "+site['name']
+    message = message + "\nAttempts: "+str(retries)
+    if('ports' in site):
+        for port in site['ports']:
+            if(port in ['443']): http = 'https'
+            else: http = 'http'
+            for i in range(retries):
+                try:
+                    check_url = format_url(site['url'],http,port)
+                    down = url_down(check_url)
+                    break
+                except FunctionTimedOut:
+                    message = message + "\n[Error]: Connection timed out to URL: "+check_url
+                    down = True
+                except Exception as e:
+                    message = message + "\n[Error]: Unable to connect to URL: "+check_url
+                    message = message + "\n[Error]:" +str(e)
+                    down = True
+                if(down): message = message + "\n[Error]: Attempt "+str(i+1)
+    else:
+        for i in range(retries):
+            try:
+                down = url_down(format_url(site['url']))
+                break
+            except FunctionTimedOut:
+                message = message + "\n[Error]: Connection timed out to URL: "+site['url']
+                down = True
+            except Exception as e:
+                message = message + "\n[Error]: Unable to connect to URL: "+site['url']
+                message = message + "\n[Error]: " +str(e)
+                down = True
+            if(down): message = message + "\n[Error]: Attempt "+str(i+1)
+    if(down):
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        subject = """[ALERT]Site '{}' is down!""".format(site['name'])
+        message = message+"""\n[Error]: Time was: {}\n""".format(dt_string)
+        print("Message: ")
+        print(message)
+        if(email): send_alert(subject,message)
+        return False
+    else:
+        return True
+
+def monitor(site_list,interval=None,retries=None,email=False):
+    if(not retries): retries = 1
+    if(interval):
+        print("Running in continuous mode.")
+        while(True):
+            for site in site_list:
+                check_site(site,retries,email)
+            sleep(interval) 
+    else:
+        print("Running once.")
+        for site in site_list:
+            check_site(site,retries,email)
+                
 if(__name__=="__main__"):
-    interval = 60
-    parser = argparse.ArgumentParser(description="""Uptime check version {}""".format(version))
-    parser.add_argument("-V", "--version", help="program version", action="store_true")
-    parser.add_argument("-t", "--test", help="test mode", action="store_true")
-    parser.add_argument("-i", "--interval", help="interval in seconds")
-    args = parser.parse_args()
-    if(args.test):
-        print("Running in test mode")
-        exit(0)
-    elif(args.version):
-        print("Running Twitter Bot Version %s"%(version))
-    elif(args.interval):
-        print("Setting interval to: "+args.interval)
-        interval = int(args.interval)
-    sites = load_sites()
-    monitor(sites,interval)
+    print(sys.argv)
+    interval = None
+    retries = None
+    if(len(sys.argv)==2):
+        print("Running in single-site mode.")
+        url = sys.argv[1]
+        sites=[
+            {
+            "name":url,
+            "url":url
+            }
+        ]
+        monitor(sites,None,1,False)
+    else:
+        print("Checking sites in specified sites file.")
+        parser = argparse.ArgumentParser(description="""Uptime check version {}""".format(version))
+        parser.add_argument("-V", "--version", help="program version", action="store_true")
+        parser.add_argument("-t", "--test", help="test mode", action="store_true")
+        parser.add_argument("-i", "--interval", help="interval in seconds")
+        parser.add_argument("-u", "--url", help="url")
+        parser.add_argument("-r", "--retries", help="Number of retries")
+        args = parser.parse_args()
+        print(args)
+        if(args.test):
+            print("Running in test mode")
+            exit(0)
+        elif(args.version):
+            print("Running Twitter Bot Version %s"%(version))
+        elif(args.interval):
+            print("Setting interval to: "+args.interval)
+            interval = int(args.interval)
+        elif(args.retries):
+            retries = int(args.retries)
+        sites = load_sites()
+        monitor(sites,interval,retries,True)
